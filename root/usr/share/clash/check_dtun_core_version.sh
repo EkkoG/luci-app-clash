d7d96b6635343836b71bbbbac8349ffeeb4a2280
cielpy <beijiu572@gmail.com>
2021-07-01 22:57:46 +0800

    use new version check url

--- root/usr/share/clash/check_dtun_core_version.sh
+++ root/usr/share/clash/check_dtun_core_version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-new_clashdtun_core_version=`curl -sL "https://github.com/frainzy1477/clashdtun/tags"| grep "/frainzy1477/clashdtun/releases/"| head -n 1| awk -F "/tag/" '{print $2}'| sed 's/\">//'`
+new_clashdtun_core_version=`curl -sL "https://github.com/Dreamacro/clash/releases/tag/premium" | grep '/Dreamacro/clash/releases/tag/premium' | tail -n 1 | awk '{ print $3}' | awk -F "<" '{print $1}'`
 sleep 2
 if [ "$?" -eq "0" ]; then
 rm -rf /usr/share/clash/new_clashdtun_core_version
